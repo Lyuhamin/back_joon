db597aecf4a361f2efd9c5cf20dad217c0943087
Your Name01~ <juell0118@naver.com>
2023-04-02 00:52:55 +0900

    arithmetic

--- mian.py
+++ mian.py
@@ -1,2 +1,20 @@
 A, B = map(int, input().split())
+print(A+B)
+
+A, B = map(int, input().split())
 print(A-B)
+
+A, B = map(int, input().split())
+print(A*B)
+
+A, B = map(int, input().split())
+print(A/B)
+
+
+a, b = map(int, input().split()) #사칙연산 
+
+print(a+b) 
+print(a-b) 
+print(a*b) 
+print(int(a/b)) 
+print(a%b)
